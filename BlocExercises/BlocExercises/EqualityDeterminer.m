a205b0a66661ad3c1044c57c5f0fa4c35f761ecb
ozzieozumo <luke.d.everett@gmail.com>
2016-01-26 15:28:27 -0500

    fix assignment 10; remove nil checks; treat NSInteger as scalar

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,33 +12,23 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    if (!string1 || !string2) {
-        return NO;
-    } else {
-        
+    
     return ([string1 isEqualToString:string2]);
-    }
+    
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    if (!number1 || !number2) {
-        return NO;
-    } else {
+   
         
         return ([number1 isEqualToNumber:number2]);
-    }
     
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    if (!integer1 || !integer2) {
-        return NO;
-    } else {
-        
+    
         return (integer1 > integer2);
-    }
 
 }
 
